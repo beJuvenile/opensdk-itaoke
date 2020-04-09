865cca29bb5a8a9c48675abe2fe15810517a1533
kangliuchang <13784368101@163.com>
2020-04-09 17:05:46 +0800

    新增api

--- src/Requests/ItaokeRobotSecondLoginRequest.php
+++ src/Requests/ItaokeRobotSecondLoginRequest.php
@@ -13,15 +13,15 @@ class ItaokeRobotSecondLoginRequest implements Request
     public $requestType = 'post';
 
     //登录的微信id（首次登录不需要传，历史登录必须传） 执行登录接口返回此字段，记得保存数据库里
-    public $wxId;
+    public $wx_id;
 
     public $apiParas = array();
 
     //微信号
     public function setWxId($wxid)
     {
-        $this->wxId = $wxid;
-        $this->apiParas["wxId"] = $wxid;
+        $this->wx_id = $wxid;
+        $this->apiParas["wx_id"] = $wxid;
     }
 
 	public function getApiMethodName()
